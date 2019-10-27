f2a1882ae63f118d2e5fd86d533f87f405036d67
BLANK-TECH <ivonne_1412@hotmail.com>
2019-10-26 23:02:07 -0500

    se edita imagen de fondo

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-//ANIME
-const img = anime({
-    targets: '#arrow',
-    translateY: 6,
-    direction: 'alternate',
-    loop: true,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-    duration: 2000
-    
-  });
-
-  function loop(t) {
-    img.tick(t);
-    customRAF = requestAnimationFrame(loop);
-  }
-  
-  requestAnimationFrame(loop);
-
-
-//SCROLL REVEAL
-
-window.sr = ScrollReveal();
-sr.reveal('#navbar',{
-  duration:2000,
-  origin:'bootom'
-});
-
 //SMOOTH SCROLLING
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
@@ -37,14 +9,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-var path = anime.path('#path');
 
-var motionPath = anime({
-  targets: '.el',
-  translateX: path('x'),
-  translateY: path('y'),
-  rotate: path('angle'),
-  easing: 'linear',
-  duration: 3000,
-  loop: true
-});
