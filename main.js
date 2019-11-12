cbac308ee16f375e89810ecdba44ef1adb2c042a
BLANK-TECH <ivonne_1412@hotmail.com>
2019-11-12 10:50:57 -0600

    slider en desktop

--- main.js
+++ main.js
@@ -23,3 +23,33 @@ const mostrarMenu = () => {
 }
 
 document.getElementById("hamburger").addEventListener("click", mostrarMenu);
+
+const sliderPokedex = document.getElementById('slider-pokedex');
+const sliderLabcar = document.getElementById('slider-labcar');
+const sliderMdLinks = document.getElementById('slider-mdLinks');
+
+document.querySelectorAll('a[href^="#slider-pokedex"]').forEach(element=>{
+    element.addEventListener("click",(e)=>{
+        e.preventDefault();
+        sliderPokedex.style.display = "block";
+        sliderMdLinks.style.display = "none";
+        sliderLabcar.style.display = "none";
+    });
+});
+document.querySelectorAll('a[href^="#slider-mdLinks"]').forEach(element=>{
+    element.addEventListener("click",(e)=>{
+        e.preventDefault();
+        sliderMdLinks.style.display = "block";
+        sliderLabcar.style.display = "none";
+        sliderPokedex.style.display = "none";
+    });
+});
+document.querySelectorAll('a[href^="#slider-labcar"]').forEach(element=>{
+    element.addEventListener("click",(e)=>{
+        e.preventDefault();
+        sliderLabcar.style.display = "block";
+        sliderPokedex.style.display = "none";
+        sliderMdLinks.style.display = "none";
+    });
+});
+
